0797bc993de4f892c54e3e749e232ec690e967a9
deepanshu188 <deepanshubisht188@gmail.com>
2025-05-09 22:09:07 +0530

    updated: reset the input filter on unmount

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,14 @@ import { StyleSheet } from 'react-native';
 import ThemedView from '../components/ThemedView';
 import ImageGrid from '../components/ImageGrid';
 import SearchInput from '../components/SearchInput';
+import { useEffect } from 'react';
+import { useFilters } from '@/store/filters';
 
 const Home = () => {
+  const { resetFilters } = useFilters()
+  useEffect(() => {
+    return () => resetFilters()
+  }, [])
   return (
     <ThemedView style={styles.container}>
       <SearchInput />
